adfcc09a382fa12d17e021d164749033fb72d047
Jack Gardner <jtgardner99@gmail.com>
2026-09-18 15:25:17 -0500

    dns configuration

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -7,6 +7,6 @@ export const testimonials = [
   {
     name: 'Avery',
     quote:
-      "Kadi has a true gift for noticing people's natural strengths and helping them bring those into their real life. You leave every conversation with her feeling more like yourself.",
+      "Kadi has a true gift for noticing your natural strengths and helping you bring those into your real life. You leave every conversation with her feeling more like yourself.",
   },
 ]
